c12dc442157510fabdf115c4c3d00a471c544aa4
Igal Kolihman <igal.kolihman@gmail.com>
2020-10-16 14:54:34 +0300

    files added

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((socket.gethostname(), 12345))
+
+    echo_data = "hello server!"
+    print(f"Sending server: {echo_data}")
+    client.send(echo_data)
+
+    echo_from_server = client.recv(1024)
+    print(f"Received from server: {echo_from_server}")
+
+    client.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ echo_server.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((socket.gethostname(), 12345))
+    server.listen(1)
+    conn, address = server.accept()
+
+    print("Waiting for a message from the client...")
+    client_data = conn.recv(1024)
+    print(f"Received from client: {client_data}")
+    conn.send(client_data)
+
+
+if __name__ == '__main__':
+    main()
